b4b86fe5b1d2131c0623877f74082edd2b068f1d
saniakrista-collab <sania.krista@gmail.com>
2026-03-13 17:37:17 +0700

    update

---
+++ EditTextButton/app/src/main/java/com/kristasania/edittextbutton/MainActivity.java
@@ -0,0 +1,43 @@
+package com.kristasania.edittextbutton;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button okButton = findViewById(R.id.okButton);
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText namaEditText = findViewById(R.id.namaEditText);
+                TextView infoTextView = findViewById(R.id.infoTextView);
+
+                String nama = namaEditText.getText().toString();
+                String pesan = "Hello, " + nama + "!";
+
+                infoTextView.setText(pesan);
+            }
+        });
+    }
+}
